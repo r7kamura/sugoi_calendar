94820127594445de1d7de299995546e99d2c724f
Ryo Nakamura <r7kamura@gmail.com>
2013-10-13 12:12:05 +0900

    POST /titles -> TitleCreateHandler

--- main.go
+++ main.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 	"github.com/r7kamura/router"
 	"net/http"
+	"strconv"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	router := router.NewRouter()
 	router.Get("/titles", http.HandlerFunc(TitleIndexHandler))
+	router.Post("/titles", http.HandlerFunc(TitleCreateHandler))
 	http.ListenAndServe(":3000", router)
 }
 
 func TitleIndexHandler(writer http.ResponseWriter, request *http.Request) {
-	records, err := dbMap.Select(Title{}, `SELECT * FROM titles ORDER BY id DESC LIMIT ?`, 1)
+	records, err := dbMap.Select(Title{}, `SELECT * FROM titles ORDER BY id DESC`)
 	if err != nil {
 		writer.WriteHeader(500)
 		return
@@ -27,4 +29,32 @@ func TitleIndexHandler(writer http.ResponseWriter, request *http.Request) {
 		writer,
 		titles,
 	)
+}
+
+func TitleCreateHandler(writer http.ResponseWriter, request *http.Request) {
+	titleParam := request.FormValue("title")
+	if titleParam == "" {
+		writer.WriteHeader(400)
+		return
+	}
+	idParam := request.FormValue("id")
+	if idParam == "" {
+		writer.WriteHeader(400)
+		return
+	}
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		writer.WriteHeader(400)
+		return
+	}
+	titleRecord := &Title{
+		ID: id,
+		Title: titleParam,
+	}
+	err = dbMap.Insert(titleRecord)
+	if err != nil {
+		writer.WriteHeader(500)
+		return
+	}
+	fmt.Fprintf(writer, "%t\n", titleRecord)
 }
